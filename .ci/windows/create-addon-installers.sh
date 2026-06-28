6b2588cdbfab3e5752031d137607080f75b1a40f
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2026-06-28 15:21:34 +0700

    ci: run add-on NSIS from staging directory

--- .ci/windows/create-addon-installers.sh
+++ .ci/windows/create-addon-installers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+shopt -s nullglob
 
 source_dir="$1"
 binary_dir="$2"
@@ -7,33 +8,32 @@ install_files="$3"
 
 cd "$binary_dir"
 
-cp -ra "${install_files}/nsis/." "${binary_dir}/nsis/"
-nsis_dir="$(cygpath -m "${binary_dir}/${install_files}/nsis")"
-sed -i \
-	-e "s#\"nsis/#\"${nsis_dir}/#g" \
-	-e "s#\"nsis\\\\#\"${nsis_dir}/#g" \
-	"${install_files}/nsis/addon-common.nsh"
 cp nsis/addon-chat.nsi "${install_files}"
 cp nsis/addon-internetaccesscontrol.nsi "${install_files}"
 cp nsis/addon-networkdiscovery.nsi "${install_files}"
 cp nsis/addon-auvidus.nsi "${install_files}"
 cp nsis/addon-screenrecorder.nsi "${install_files}"
 
-sed -i \
-	-e "s#\"nsis/#\"${nsis_dir}/#g" \
-	-e "s#\"nsis\\\\#\"${nsis_dir}/#g" \
-	"${install_files}"/addon-*.nsi
+pushd "${install_files}" > /dev/null
 
-makensis "${install_files}/addon-chat.nsi"
-makensis "${install_files}/addon-internetaccesscontrol.nsi"
-makensis "${install_files}/addon-networkdiscovery.nsi"
-makensis "${install_files}/addon-auvidus.nsi"
+makensis addon-chat.nsi
+makensis addon-internetaccesscontrol.nsi
+makensis addon-networkdiscovery.nsi
+makensis addon-auvidus.nsi
 
 if [ -f "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" ]; then
-	cp "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" "${install_files}/"
-	makensis "${install_files}/addon-screenrecorder.nsi"
+	cp "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" .
+	makensis addon-screenrecorder.nsi
 else
 	echo "SKIP ScreenRecorder installer: place an LGPL ffmpeg.exe at 3rdparty/ffmpeg/ffmpeg.exe"
 fi
 
-mv "${install_files}"/Sahid-*-Addon-*setup.exe "${binary_dir}"
+addons=( Sahid-*-Addon-*setup.exe )
+if [ "${#addons[@]}" -eq 0 ]; then
+	echo "ERROR: no add-on installers were produced by NSIS" >&2
+	exit 1
+fi
+
+mv "${addons[@]}" "${binary_dir}"
+
+popd > /dev/null
