6ba05cd41b988bf53d5bb4cf612bbd27615134e7
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2026-06-28 11:49:57 +0700

    ci: expose NSIS assets from build directory

--- .ci/windows/create-addon-installers.sh
+++ .ci/windows/create-addon-installers.sh
@@ -7,24 +7,23 @@ install_files="$3"
 
 cd "$binary_dir"
 
+cp -ra "${install_files}/nsis/." "${binary_dir}/nsis/"
 cp nsis/addon-chat.nsi "${install_files}"
 cp nsis/addon-internetaccesscontrol.nsi "${install_files}"
 cp nsis/addon-networkdiscovery.nsi "${install_files}"
 cp nsis/addon-auvidus.nsi "${install_files}"
 cp nsis/addon-screenrecorder.nsi "${install_files}"
 
-pushd "${install_files}" > /dev/null
-
-makensis addon-chat.nsi
-makensis addon-internetaccesscontrol.nsi
-makensis addon-networkdiscovery.nsi
-makensis addon-auvidus.nsi
+makensis "${install_files}/addon-chat.nsi"
+makensis "${install_files}/addon-internetaccesscontrol.nsi"
+makensis "${install_files}/addon-networkdiscovery.nsi"
+makensis "${install_files}/addon-auvidus.nsi"
 
 if [ -f "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" ]; then
-	cp "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" .
-	makensis addon-screenrecorder.nsi
+	cp "${source_dir}/3rdparty/ffmpeg/ffmpeg.exe" "${install_files}/"
+	makensis "${install_files}/addon-screenrecorder.nsi"
 else
 	echo "SKIP ScreenRecorder installer: place an LGPL ffmpeg.exe at 3rdparty/ffmpeg/ffmpeg.exe"
 fi
 
-mv Sahid-*-Addon-*setup.exe "${binary_dir}"
+mv "${install_files}"/Sahid-*-Addon-*setup.exe "${binary_dir}"
--- .ci/windows/create-base-installer.sh
+++ .ci/windows/create-base-installer.sh
@@ -7,15 +7,14 @@ install_files="$2"
 
 cd "$binary_dir"
 
-pushd "${install_files}" > /dev/null
-makensis veyon.nsi
+cp -ra "${install_files}/nsis/." "${binary_dir}/nsis/"
+makensis "${install_files}/veyon.nsi"
 
-installers=( veyon-*setup.exe )
+installers=( "${install_files}"/veyon-*setup.exe )
 if [ "${#installers[@]}" -eq 0 ]; then
 	echo "ERROR: base installer was not produced by NSIS" >&2
 	exit 1
 fi
 
 mv "${installers[@]}" "${binary_dir}"
-popd > /dev/null
 rm -rf "${install_files}"
