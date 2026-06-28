38296ab8605ba74fc1796c47318a8e5e6693de59
rizky herdiansyah <rizkyherdiansyah31@gmail.com>
2026-06-28 15:42:20 +0700

    ci: use relative NSIS assets for base installer

--- .ci/windows/create-base-installer.sh
+++ .ci/windows/create-base-installer.sh
@@ -7,15 +7,19 @@ install_files="$2"
 
 cd "$binary_dir"
 
+mkdir -p nsis
 cp -ra "${install_files}/nsis/." "${binary_dir}/nsis/"
-nsis_dir="$(cygpath -m "${binary_dir}/${install_files}/nsis")"
-sed -i \
-	-e "s#\"nsis/#\"${nsis_dir}/#g" \
-	-e "s#\"nsis\\\\#\"${nsis_dir}/#g" \
-	"${install_files}/veyon.nsi"
+
+for asset in nsis/welcome-page.bmp nsis/header.bmp nsis/installer.ico nsis/uninstaller.ico; do
+	if [ ! -f "${asset}" ]; then
+		echo "ERROR: required NSIS asset is missing: ${asset}" >&2
+		exit 1
+	fi
+done
+
 makensis "${install_files}/veyon.nsi"
 
-installers=( "${install_files}"/veyon-*setup.exe )
+installers=( "${install_files}"/veyon-*setup.exe veyon-*setup.exe )
 if [ "${#installers[@]}" -eq 0 ]; then
 	echo "ERROR: base installer was not produced by NSIS" >&2
 	exit 1
